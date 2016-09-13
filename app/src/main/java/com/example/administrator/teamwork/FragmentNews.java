1f7b33504160dc7335b8bd53d2bdeebc24d59b6e
Administrator <854953241@qq.com>
2016-09-13 14:25:27 +0800

    003

--- app/src/main/java/com/example/administrator/teamwork/FragmentNews.java
+++ app/src/main/java/com/example/administrator/teamwork/FragmentNews.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 /**
  * Created by Administrator on 2016/9/12.
  */
-public class FragmentNews extends Fragment {
+public class FragmentNews extends  Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
