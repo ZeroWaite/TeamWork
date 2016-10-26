4932cf69d846b0ba84171f55acc51c2895181941
luo <shadow3030@163.com>
2016-10-26 15:51:42 +0800

    luo

---
+++ app/src/main/java/com/example/administrator/teamwork/Login_Regist/MyUser.java
@@ -0,0 +1,10 @@
+package com.example.administrator.teamwork.Login_Regist;
+
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Created by anzhuo on 2016/10/25.
+ */
+public class MyUser extends BmobUser {
+
+}
